ff87e43d707b2ca29e484fdf3f268d28ade682b3
David Ordovskiy <david.ordovsky@gmail.com>
2017-10-24 09:28:28 -0400

    set up boilerplate project

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const tempJSX = (
+  <p>Hi, I'm App!</p>
+);
+
+ReactDOM.render(tempJSX, document.getElementById('app'));
