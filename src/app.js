970c5d074147b0ae7751b906e6352ffb1ae67a3d
David Ordovskiy <david.ordovsky@gmail.com>
2017-10-24 13:58:07 -0400

    set up basic routes and dummy components

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const tempJSX = (
-  <p>Hi, I'm App!</p>
+const DashboardPage = () => (
+  <div>
+    The Dashboard Page
+  </div>
 );
 
-ReactDOM.render(tempJSX, document.getElementById('app'));
+const AddExpensePage = () => (
+  <div>
+    Add expenses here
+  </div>
+);
+
+const EditExpensesPage = () => (
+  <div>
+    Edit expenses
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    Not sure if we can help you yet...
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={DashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensesPage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
