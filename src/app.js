819207e5ba0e1766bb368f868023609eea0b0733
David Ordovskiy <david.ordovsky@gmail.com>
2017-10-25 08:21:41 -0400

    convert all links to navlinks

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -31,19 +31,32 @@ const HelpPage = () => (
 
 const NotFound = () => (
   <div>
-    Four Oh Four...
+    Four Oh Four... <Link to="/">Go home</Link>
   </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Enter expenses</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit expenses</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Haaaaaalp!</NavLink>
+  </header>
 );
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={DashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensesPage} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={NotFound} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={DashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensesPage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
