651c734f688ab04dd0dd859f30c614dc6e6f48f9
David Ordovskiy <david.ordovsky@gmail.com>
2017-10-31 08:15:31 -0400

    fix sortBy dropdown

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -12,7 +12,9 @@ const ExpenseListFilters = (props) => (
         props.dispatch(setTextFilter(e.target.value))
       }}
     />
-    <select onChange={(e) => {
+    <select
+      value={props.filters.sortBy}
+      onChange={(e) => {
       if (e.target.value === 'date') {
         props.dispatch(sortByDate())
       } else if (e.target.value === 'amount') {
