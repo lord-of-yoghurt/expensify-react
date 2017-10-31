b8aee3f31d481bf1d6be7308d733cf5855da3941
David Ordovskiy <david.ordovsky@gmail.com>
2017-10-31 19:52:10 -0400

    set up functionality to edit an expense, place the 'remove' button in EditExpense component

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const EditExpense = (props) => {
-  console.log(props);
+import Form from './ExpenseForm';
+
+import { editExpense, removeExpense } from '../actions/expenses';
+
+const EditExpense = props => {
   return (
     <div>
-      Editing the expense with id of {props.match.params.id}
+      <Form
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(editExpense(props.expense.id, expense));
+          props.history.push('/');
+        }}
+      />
+      <button onClick={() => {
+        props.dispatch(removeExpense({ id: props.expense.id }));
+        props.history.push('/');
+      }}>
+        Remove
+      </button>
     </div>
   );
 };
 
-export default EditExpense;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expenses.find(expense => expense.id === props.match.params.id)
+  };
+};
+
+export default connect(mapStateToProps)(EditExpense);
