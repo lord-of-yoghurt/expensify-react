3f07d78b2de207627d7a801df372f66467a72241
David Ordovskiy <david.ordovsky@gmail.com>
2017-11-01 13:47:41 -0400

    set up tests for expenses actions

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,3 +1,61 @@
-test('should do something', () => {
-  expect(true).toBeTruthy();
+import {
+  addExpense,
+  editExpense,
+  removeExpense,
+  ADD_EXPENSE,
+  EDIT_EXPENSE,
+  REMOVE_EXPENSE
+} from '../../actions/expenses';
+
+const testExpense = {
+  description: 'rent',
+  amount: '180000',
+  note: 'i hate rent',
+  createdAt: -61000
+};
+
+test('returns a valid addExpense action object with provided values', () => {
+  const action = addExpense(testExpense);
+
+  expect(action).toEqual({
+    type: ADD_EXPENSE,
+    expense: {
+      ...testExpense,
+      id: expect.any(String)
+    }
+  });
+});
+
+test('returns a valid addExpense action object with default values', () => {
+  const action = addExpense();
+
+  expect(action).toEqual({
+    type: ADD_EXPENSE,
+    expense: {
+      id: expect.any(String),
+      description: '',
+      amount: 0,
+      note: '',
+      createdAt: 0
+    }
+  });
+});
+
+test('returns a valid editExpense action object', () => {
+  const action = editExpense('some-id', testExpense);
+
+  expect(action).toEqual({
+    type: EDIT_EXPENSE,
+    id: 'some-id',
+    updates: testExpense
+  });
+});
+
+test('returns a valid removeExpense action object', () => {
+  const action = removeExpense({ id: 'some-id'});
+
+  expect(action).toEqual({
+    type: REMOVE_EXPENSE,
+    id: 'some-id'
+  });
 });
