1ca8016c383fb40b21111c6e78204ed8ce05ef51
David Ordovskiy <david.ordovsky@gmail.com>
2017-11-02 08:46:57 -0400

    set up tests for filters actions

---
+++ src/tests/actions/filters.test.js
@@ -0,0 +1,68 @@
+import moment from 'moment';
+
+import {
+  SET_TEXT_FILTER,
+  SET_START_DATE,
+  SET_END_DATE,
+  SORT_BY_AMOUNT,
+  SORT_BY_DATE,
+  setTextFilter,
+  setStartDate,
+  setEndDate,
+  sortByAmount,
+  sortByDate
+} from '../../actions/filters';
+
+const testDate = moment(0);
+
+test('generates setStartDate action object', () => {
+  const action = setStartDate(testDate);
+
+  expect(action).toEqual({
+    type: SET_START_DATE,
+    startDate: testDate
+  });
+});
+
+test('generates setEndDate action object', () => {
+  const action = setEndDate(testDate);
+
+  expect(action).toEqual({
+    type: SET_END_DATE,
+    endDate: testDate
+  });
+});
+
+test('generates sortByAmount action object', () => {
+  const action = sortByAmount();
+
+  expect(action).toEqual({
+    type: SORT_BY_AMOUNT
+  });
+})
+
+test('generates sortByDate action object', () => {
+  const action = sortByDate();
+
+  expect(action).toEqual({
+    type: SORT_BY_DATE
+  });
+})
+
+test('generates setTextFilter action to clear text filter', () => {
+  const action = setTextFilter();
+
+  expect(action).toEqual({
+    type: SET_TEXT_FILTER,
+    text: ""
+  });
+});
+
+test('generates setTextFilter action to set new filter', () => {
+  const action = setTextFilter('coffee');
+
+  expect(action).toEqual({
+    type: SET_TEXT_FILTER,
+    text: 'coffee'
+  });
+});
