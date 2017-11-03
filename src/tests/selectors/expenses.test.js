253ae880b7a43204d788e24028f1f7bbb27465fc
David Ordovskiy <david.ordovsky@gmail.com>
2017-11-03 18:23:01 -0400

    set up tests for expenses reducer

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -2,29 +2,7 @@ import moment from 'moment';
 
 import selectExpenses from '../../selectors/expenses';
 
-const expenses = [
-  {
-    id: '1',
-    description: 'rent',
-    amount: 100,
-    note: 'i wish',
-    createdAt: 0
-  },
-  {
-    id: '2',
-    description: 'coffee',
-    amount: 1500,
-    note: '...',
-    createdAt: moment(0).subtract(4, 'days').valueOf()
-  },
-  {
-    id: '3',
-    description: 'spotify membership',
-    amount: 999,
-    note: '',
-    createdAt: moment(0).add(7, 'days').valueOf()
-  },
-];
+import expenses from '../fixtures/expenses';
 
 const defaultFilters = {
   text: '',
