47ffa6c4b8f40d42d941e95248b16260999e6c4b
David Ordovskiy <david.ordovsky@gmail.com>
2017-11-09 08:33:55 -0500

    remove test code from app.js

--- src/app.js
+++ src/app.js
@@ -21,22 +21,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.subscribe(() => {
-  const state = store.getState(),
-        visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-  // console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({ description: 'Electric bill', amount: 500, createdAt: -21000 }));
-store.dispatch(addExpense({ description: 'Phone bill', amount: 1300, createdAt: 12000 }));
-store.dispatch(addExpense({ description: 'Lunch', amount: 1000, createdAt: -1000 }));
-store.dispatch(addExpense({ description: 'Coke Zero', amount: 800, createdAt: -60000 }));
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter())
-// }, 3000);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
