23a709c0fb7a621c16c193fae442258ab0bcfa97
David Ordovskiy <david.ordovsky@gmail.com>
2017-11-10 09:44:22 -0500

    make settings for heroku deploy

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 const express = require('express'),
       path = require('path'),
       app = express(),
-      publicPath = path.join(__dirname, '..', 'public');
+      publicPath = path.join(__dirname, '..', 'public'),
+      port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
-});
-
-app.listen(3000, () => {
-  console.log('\n>>>>> http://localhost:3000');
 });
