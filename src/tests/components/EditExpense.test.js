67d1845a42fdc14aae1db73cf9b36bf68e4608b0
David Ordovskiy <david.ordovsky@gmail.com>
2017-11-16 17:44:33 -0500

    fix addExpense/editExpense tests with new routes

--- src/tests/components/EditExpense.test.js
+++ src/tests/components/EditExpense.test.js
@@ -27,13 +27,13 @@ test('renders EditExpense correctly', () => {
 test('handles editing an expense correctly', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[0]);
 
-  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(history.push).toHaveBeenLastCalledWith('/dashboard');
   expect(startEditExpense).toHaveBeenLastCalledWith(expenses[0].id, expenses[0]);
 });
 
 test('handles removing an expense correctly', () => {
   wrapper.find('button').simulate('click');
 
-  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(history.push).toHaveBeenLastCalledWith('/dashboard');
   expect(startRemoveExpense).toHaveBeenLastCalledWith({ id: expenses[0].id });
 });
