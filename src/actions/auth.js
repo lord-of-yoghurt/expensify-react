c504662108dbb54eaf9df5686f37dc330d79223c
David Ordovskiy <david.ordovsky@gmail.com>
2017-11-17 22:43:13 -0500

    add auth reducer and login/logout actions, wire everything up

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,21 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase';
 
+import * as co from '../constants/auth';
+
+export const login = (uid) => ({
+  type: co.LOGIN,
+  uid
+});
+
 export const startLogin = () => {
   return () => {
     return firebase.auth().signInWithPopup(googleAuthProvider);
   };
 };
+
+export const logout = () => ({
+  type: co.LOGOUT
+});
 
 export const startLogout = () => {
   return () => {
--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import { Provider } from 'react-redux';
 // redux imports
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 
 // style imports
@@ -43,6 +43,7 @@ ReactDOM.render(<p>Computing...</p>, document.getElementById('app'));
 firebase.auth().onAuthStateChanged((user) => {
   // user logged in
   if (user) {
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
@@ -51,6 +52,7 @@ firebase.auth().onAuthStateChanged((user) => {
     });
   // user logged out
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push('/');
   }
