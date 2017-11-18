fd405f2bcaf464b0a914daddbdae1cb19bcaacfe
David Ordovskiy <david.ordovsky@gmail.com>
2017-11-18 17:17:51 -0500

    create PrivateRoute wrapper for Route and add it to the app

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,22 +5,21 @@ import createHistory from 'history/createBrowserHistory';
 import AddExpense from '../components/AddExpense';
 import Dashboard from '../components/Dashboard';
 import EditExpense from '../components/EditExpense';
-import Header from '../components/Header';
 import Help from '../components/Help';
 import LoginPage from '../components/LoginPage';
 import NotFound from '../components/NotFound';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
         <Route path="/" component={LoginPage} exact={true} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/create" component={AddExpense} />
-        <Route path="/edit/:id" component={EditExpense} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/create" component={AddExpense} />
+        <PrivateRoute path="/edit/:id" component={EditExpense} />
         <Route path="/help" component={Help} />
         <Route component={NotFound} />
       </Switch>
